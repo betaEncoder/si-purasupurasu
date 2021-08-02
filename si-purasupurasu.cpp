f3162a54d336a3e403c1a62411159066cc907cde
betaEncoder <maruyama@mirai-atelier.com>
2021-08-03 01:57:10 +0900

    イニシャルコミット

---
+++ si-purasupurasu.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "si-purasupurasu.h"
+
+イント メイン()
+{
+    エステーデー::コンソールアウト << "ハローワールド!\n";
+
+    リターン 0;
+}
---
+++ si-purasupurasu.h
@@ -0,0 +1,10 @@
+#pragma once
+#define デファイン define
+#define インクルード include
+#define メイン main
+#define リターン return
+#define エステーデー std
+#define コンソールアウト cout
+#define イント int
+#define イフ if
+#define エルス else
